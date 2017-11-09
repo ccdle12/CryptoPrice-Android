82518d074f358ca8d4b9f840322b99b30e2c0bc9
ccdle12 <chris.coverdale24@gmail.com>
2017-11-09 13:56:38 +0000

    Created tests for coinbase exchange

--- app/src/test/java/com/example/christophercoverdale/cryptoprice_android/CoinbaseTest.java
+++ app/src/test/java/com/example/christophercoverdale/cryptoprice_android/CoinbaseTest.java
@@ -5,19 +5,25 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import CustomClasses.Coins.CryptoCurrency;
 import CustomClasses.Exchanges.Coinbase;
+import Managers.PFDataManager;
 
 /**
  * Created by christophercoverdale on 03/11/2017.
  */
 public class CoinbaseTest
 {
+    PFDataManager pfDataManager;
     Coinbase coinbase;
 
     @Before
     public void setUpObject()
     {
+        this.pfDataManager = new PFDataManager();
         this.coinbase = new Coinbase();
+
+        this.coinbase.setDelegate(this.pfDataManager);
     }
 
     @Test
@@ -30,6 +36,23 @@ public class CoinbaseTest
     public void canAccessBitcoinPrice()
     {
         Assert.assertEquals(-1.0, this.coinbase.bitcoin.USDPrice);
+    }
+
+    @Test
+    public void retrievingDataCorrectly()
+    {
+        this.coinbase.exchangeUpdatePrice("Coinbase", 0);
+
+        CryptoCurrency updatedCoin = this.coinbase.coins.get(0);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(updatedCoin.USDPrice > 0);
     }
 
 
